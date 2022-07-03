0e0f49647c72489748a2e8cb5f97ab4486daa517
Ankit Kumar <address.ankit@gmail.com>
2022-07-04 02:27:08 +0530

    (final-1) compilation

--- assembler/cmderror.py
+++ assembler/cmderror.py
@@ -87,7 +87,7 @@ def checking_typeB(cmd,dicinst,dicreg,inlist,q,s,count):
                 print(f"Error At line {list(inslist.keys())[count]} : Number out of range")
                 return False
 
-def checking_typeC(cmd,dicinst,dicreg,q,s,count):
+def checking_typeC(cmd,dicinst,dicreg,inslist,q,s,count):
     if((cmd[1] not in dicreg.keys() or cmd[1]=="FLAGS") or cmd[2] not in dicreg.keys()):
         if(q==1):
             print(f"Error At line {list(inslist.keys())[count]} : Invalid Register in Label")
@@ -209,7 +209,7 @@ def checking_label(s,diclabel,inslist,q,count):
     x = idx+2
     y = idx-1
     if(idx==0 or idx==len(s)-1 or s[x]==' ' or s[y]==' '):
-        print(f"Error At line {count} : Invalid label syntax")
+        print(f"Error At line {list(inslist.keys())[count]} : Invalid label syntax")
         return False
     else:
         q=1
@@ -220,19 +220,71 @@ def checking_label(s,diclabel,inslist,q,count):
 
 
 q = 0
-dicinst = {"add":['10000','A'],"sub":['10001','B'],"mov":['10010','B'],"mov":['10011','C'],"ld":['10100','D'],
+dicinst = {"add":['10000','A'],"sub":['10001','A'],"mov":['10010','B'],"mov":['10011','C'],"ld":['10100','D'],
 "st":['10101','D'],"mul":['10110','A'],"div":['10111','C'],"rs":['11000','B'],"ls":['11001','B'],"xor":['11010','A'],
 "or":['11011','A'],"and":['11100','A'],"not":['11101','C'],"cmp":['11110','C'],"jmp":['11111','E'],
 "jlt":['01100','E'],"jgt":['01101','E'],"je":['01111','E'],"hlt":['01010','F']}
 
-dicreg = {'R1': "000",'R2':"001",'R3':"010","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
+dicreg = {"R0" : "000",
+       "R1" : "001",
+        "R2": "010",
+        "R3": "011",
+        "R4": "100",
+        "R5": "101",
+        "R6": "110",
+        "FLAGS": "111",}
 # diclabel = {'label1':3}
 
-inslist= {0: 'var X', 1: 'var y', 2: 'var Z', 4: 'var u', 6: 'add R1 R2 R3', 7: 'add R1 R2 R3', 9: 'add R1 R2 R3', 13: 'mov R1 $9', 18: 'add: add R3 R3 R4', 20: 'hlt'}
-instDict={0: 'add R1 R2 R3', 1: 'add R1 R2 R3', 2: 'add R1 R2 R3', 3: 'mov R1 $9', 4: 'add: add R3 R3 R4', 5: 'hlt'}
-varDict= {6: 'X', 7: 'y', 8: 'Z', 9: 'u'}
-diclabel= {18: 'add'}
-    
+# inslist= {0: 'var X', 1: 'var y', 2: 'var Z', 4: 'var u', 6: 'add R1 R2 R3', 7: 'add R1 R2 R3', 9: 'add R1 R2 R3', 13: 'mov R1 $9', 18: 'add: add R3 R3 R4', 20: 'hlt'}
+# instDict={0: 'add R1 R2 R3', 1: 'add R1 R2 R3', 2: 'add R1 R2 R3', 3: 'mov R1 $9', 4: 'add: add R3 R3 R4', 5: 'hlt'}
+# varDict= {6: 'X', 7: 'y', 8: 'Z', 9: 'u'}
+# diclabel= {18: 'add'}
+
+
+
+input_file = open("1112.txt","r")
+a = input_file.read().split("\n")
+print(a)
+inslist = {}
+for i in range(1,len(a)+1):
+    if a[i-1] == '' or '\t' in a[i-1] :
+        pass
+    else:
+        inslist[i] = a[i-1]
+purified = []
+for line in inslist.values():
+    if line.split()[0] == "var":
+        pass
+    else:
+        purified.append(line)
+instDict = {}
+for i in range(1,len(purified)+1):
+    instDict[i]= purified[i-1]
+x = len(instDict)
+varDict = {}
+f = list(inslist.values())
+for i in range(1,len(f)+1):
+    if f[i-1].split()[0] == 'var' and len(f[i-1].split()) == 2:
+        varDict[i+x] = f[i-1].split()[1]
+    else:
+        break
+diclabel = {}
+iterator = 0
+for i in inslist.values():
+    if ":" in i and (i[0] !=":" or i[-1] != ":"):
+        diclabel[list(inslist.keys())[iterator]] = i[0:i.index(":")]
+    iterator+=1
+varDicta = {value:key for key, value in varDict.items()}
+diclabela = {value:key for key, value in diclabel.items()}
+
+print(instDict)
+print(varDict)
+print(inslist)
+
+
+
+final=[]
+
 # print(inslist.keys())
 count = len(varDict.values())
 for i in instDict.values():
@@ -242,14 +294,20 @@ for i in instDict.values():
             if(checking_label(i,diclabel,inslist,q,count)==False):
                 break
             else:
-                print(i)
-                print(convertor(i.split()[1:], varDict, diclabel))
+                
+                final.append(convertor(i.split()[1:], varDicta, diclabela))
     else:
         cmd = i.split()
         if(checking(cmd,dicinst,dicreg,varDict,diclabel,inslist,q,i,count)==False):
             break
         
         else:
-            print(i)
-            print(convertor(i.split(), varDict, diclabel))
+            
+            final.append(convertor(i.split(), varDicta, diclabela))
     count+=1
+
+
+with open("hehe.txt", 'w') as g:
+    for i in range(len(final)):
+        s=final[i]+'\n'
+        g.write(s)
