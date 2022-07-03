c2194faf85c466090b481b0d22716c17e2576363
Ayush21031 <108355333+Ayush21031@users.noreply.github.com>
2022-07-03 12:58:43 +0530

    partial label error handled

--- assembler/cmderror.py
+++ assembler/cmderror.py
@@ -67,7 +67,7 @@ def checking_typeD(cmd,dicinst,dicreg,varDict,count):
         else:
             return True
 
-def checking_typeE(cmd,dicinst,dicreg,diclabel,count):
+def checking_typeE(cmd,dicinst,dicreg,diclabel,inslist):
     if(cmd[1] not in diclabel.keys()):
         print(f"Error At line {inslist[' '.join(cmd)]} : Invalid label")
         return False
@@ -77,12 +77,21 @@ def checking_typeE(cmd,dicinst,dicreg,diclabel,count):
 def checking_typeF(cmd,dicinst,dicreg,diclabel):
     return True
 
-def checking(cmd,dicinst,dicreg,varDict,diclabel,inslist):
+
+
+
+
+def checking(cmd,dicinst,dicreg,varDict,diclabel,inslist,q,lab):
     if(list(inslist.keys())[-1]=='hlt'):
 
         if(cmd[0] not in dicinst.keys() or cmd[0]=='var'):
-            print(f"Error At line {inslist[' '.join(cmd)]} : Invalid Instruction")
-            return False
+            if(q==1):
+                print(f"Error At line {inslist[lab]} : Invalid Label")
+                q=0
+            else:
+                print(cmd)
+                print(f"Error At line {inslist[' '.join(cmd)]} : Invalid Instruction")
+                return False
 
         else:
             if(cmd[0]=='mov'):
@@ -128,22 +137,40 @@ def checking(cmd,dicinst,dicreg,varDict,diclabel,inslist):
         return False
 
 
+def checking_label(s,diclabel,q):
+    idx = s.index(':')
+    x = idx+2
+    y = idx-1
+    if(idx==0 or idx==len(s)-1 or s[x]==' ' or s[y]==' '):
+        print(f"Error At line {inslist[s]} : Invalid label syntax")
+        return False
+    else:
+        q=1
+        o = list(s.split())[1:]
+        k = checking(o,dicinst,dicreg,varDict,diclabel,inslist,q,s)
+        return k
 
+
+q = 0
 dicinst = {"add":['10000','A'],"sub":['10001','B'],"mov":['10010','B'],"mov":['10011','C'],"ld":['10100','D'],
 "st":['10101','D'],"mul":['10110','A'],"div":['10111','C'],"rs":['11000','B'],"ls":['11001','B'],"xor":['11010','A'],
 "or":['11011','A'],"and":['11100','A'],"not":['11101','C'],"cmp":['11110','C'],"jmp":['11111','E'],
 "jlt":['01100','E'],"jgt":['01101','E'],"je":['01111','E'],"hlt":['01010','F']}
 
 dicreg = {'R1': "000",'R2':"001",'R3':"010","R4":"100","R5":"101","R6":"110"}
-diclabel = {}
+diclabel = {'label1:':3}
 
-inslist = {'var X':1, 'var y':2, 'var Z':3, 'var u':4, 'add R1 R2 R3':5, 'mov R1 $9':6,'hlt':7}
-instDict = {'add R1 R2 R3': 1, 'mov R1 $9': 2,'hlt':3}
-varDict = {'X': 3, 'y': 4, 'Z': 5, 'u': 6}
+inslist = {'var X':1, 'var y':2, 'var Z':3, 'var u':4, 'add R1 R2 R3':5, 'mov R1 $9':6,'label1: add R1 R2 R3':7,'hlt':8}
+instDict = {'add R1 R2 R3': 1, 'mov R1 $9': 2,'label1: add R1 R2 R3':3,'hlt':4}
+varDict = {'X': 5, 'y': 6, 'Z': 7, 'u': 8}
 
 # print(inslist.keys())
 
 for i in instDict.keys():
+    if(':' in i):
+        if(checking_label(i,diclabel,q)==False):
+            break
+    else:
         cmd = i.split()
-        if(checking(cmd,dicinst,dicreg,varDict,diclabel,inslist)==False):
+        if(checking(cmd,dicinst,dicreg,varDict,diclabel,inslist,q,i)==False):
             break
